9eb21e0b52b593ffa538270126c713dcd79b0170
somename <someemail>
2015-10-15 13:18:37 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -56,13 +56,13 @@ public class BowlingGame {
 				{
 					TotalScore += frames.get(i+1).GetFirstThrow();
 				}
-				//else
-				//{
-					//Frame framebonus = new Frame (7,0);
-					//addFrame(framebonus);
-					//TotalScore += 7;// frames.get(i+1).GetFirstThrow();
-					//i = frames.size();
-				//}
+				else
+				{
+					Frame framebonus = new Frame (7,0);
+					addFrame(framebonus);
+					TotalScore += frames.get(i+1).GetFirstThrow();
+					i = frames.size();
+				}
 			}
 		}
 		return TotalScore;
