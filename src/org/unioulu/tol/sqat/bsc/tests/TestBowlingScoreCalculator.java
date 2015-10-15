7d6faf27f890bb5b4859c497c1d86208ee08cb45
somename <someemail>
2015-10-15 12:20:45 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -45,5 +45,33 @@ public class TestBowlingScoreCalculator {
 		bowlinggame.addFrame(frame10);
 		assertEquals(10,bowlinggame.Getframes().size());
 	}
+	
+	@Test
+	public void testIfGameScoreIsCorrect() {
+		Frame frame1 = new Frame(1,5); //6
+		Frame frame2 = new Frame(3,6); //9
+		Frame frame3 = new Frame(7,2); //9
+		Frame frame4 = new Frame(3,6); // 9
+		Frame frame5 = new Frame(4,4); //8
+		Frame frame6 = new Frame(5,3); // 8
+		Frame frame7 = new Frame(3,3); //6
+		Frame frame8 = new Frame(4,5); // 9
+		Frame frame9 = new Frame(8,1); //9
+		Frame frame10 = new Frame(2,6); //8
+										//15,24,31,39,37,45,51,60,69,78
+		BowlingGame bowlinggame = new BowlingGame();
+		bowlinggame.addFrame(frame1);
+		bowlinggame.addFrame(frame2);
+		bowlinggame.addFrame(frame3);
+		bowlinggame.addFrame(frame4);
+		bowlinggame.addFrame(frame5);
+		bowlinggame.addFrame(frame6);
+		bowlinggame.addFrame(frame7);
+		bowlinggame.addFrame(frame8);
+		bowlinggame.addFrame(frame9);
+		bowlinggame.addFrame(frame10);
+		
+		assertEquals(78, bowlinggame.score());
+	}
 
 }
