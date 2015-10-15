b4260fee759db8a9d6cd19b73d70a74c56604c25
somename <someemail>
2015-10-15 13:32:18 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -20,25 +20,25 @@ public class BowlingGame {
 	
 	public int score(){
 		int TotalScore = 0;
-		for(int i = 0; i < frames.size(); i++)
+		for(int i = 0; i < 10; i++)
 		{
 		TotalScore += frames.get(i).score();
 			if(frames.get(i).isStrike() == true)
 			{
-				if(i+1 < frames.size())
+				if(i+1 < 10)
 				{
 					if(frames.get(i+1).isStrike() == true)
 					{
 						TotalScore += frames.get(i+1).score();
 						TotalScore += frames.get(i+2).GetFirstThrow();
 						int temp = 0;
-						for(int x = i+2; x < frames.size() && frames.get(x).isStrike() == true; x++)
+						for(int x = i+2; x < 10 && frames.get(x).isStrike() == true; x++)
 						{
 							TotalScore += frames.get(x+1).GetFirstThrow();
 							temp = x;
 						}
 						System.out.println(temp);
-						if(temp > 0 && temp < frames.size() &&frames.get(temp).isStrike() != true && frames.get(temp-1).isStrike() == true)
+						if(temp > 0 && temp < 10 &&frames.get(temp).isStrike() != true && frames.get(temp-1).isStrike() == true)
 						{
 							TotalScore += frames.get(temp).GetFirstThrow();
 						}
@@ -52,24 +52,24 @@ public class BowlingGame {
 			}
 			if(frames.get(i).isStrike() == true)
 			{
-				if(i+1 >= frames.size())
+				if(i+1 >= 10))
 				{
-				Frame framebonus = new Frame (7,2);
-				addFrame(framebonus);
+				//Frame framebonus = new Frame (7,2);
+				//addFrame(framebonus);
 				TotalScore += frames.get(i+1).score();
 				i = frames.size();
 				}
 			}
 			else if(frames.get(i).isSpare() == true)
 			{
-				if(i+1 < frames.size())
+				if(i+1 < 10)
 				{
 					TotalScore += frames.get(i+1).GetFirstThrow();
 				}
 				else
 				{
-					Frame framebonus = new Frame (7,0);
-					addFrame(framebonus);
+					//Frame framebonus = new Frame (7,0);
+					//addFrame(framebonus);
 					TotalScore += frames.get(i+1).GetFirstThrow();
 					i = frames.size();
 				}
