d44ac577649bb9ad23a41aa7c52f45d6b2143f09
somename <someemail>
2015-10-15 13:07:02 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -163,6 +163,35 @@ public class TestBowlingScoreCalculator {
 		
 		assertEquals(112, bowlinggame.score());
 	}
+	@Test
+	public void testIfTwoSpareInARowScoreisCalculatedCorrectly() {
+		Frame frame1 = new Frame(8,2); //10
+		Frame frame2 = new Frame(5,5); //10
+		Frame frame3 = new Frame(7,2); //9
+		Frame frame4 = new Frame(3,6); // 9
+		Frame frame5 = new Frame(4,4); //8
+		Frame frame6 = new Frame(5,3); // 8
+		Frame frame7 = new Frame(3,3); //6
+		Frame frame8 = new Frame(4,5); // 9
+		Frame frame9 = new Frame(8,1); //9
+		Frame frame10 = new Frame(2,6); //8
+										//85
+										//+next throw
+										//=98
+		BowlingGame bowlinggame = new BowlingGame();
+		bowlinggame.addFrame(frame1);
+		bowlinggame.addFrame(frame2);
+		bowlinggame.addFrame(frame3);
+		bowlinggame.addFrame(frame4);
+		bowlinggame.addFrame(frame5);
+		bowlinggame.addFrame(frame6);
+		bowlinggame.addFrame(frame7);
+		bowlinggame.addFrame(frame8);
+		bowlinggame.addFrame(frame9);
+		bowlinggame.addFrame(frame10);
+		
+		assertEquals(98, bowlinggame.score());
+	}
 	
 	
 	
